f8927744907b2a02f4d32a5c6a3329a446630025
Алексей Белянин <alekseybelyanin@MacBook-Pro-Aleksej.local>
2017-09-17 16:57:38 +0300

    fix

--- лишай/main.cpp
+++ лишай/main.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 
 class cell;
+class material;
 
 class coordinate {
 public:
@@ -15,11 +16,11 @@ public:
 
 class node {
 public:
-    cell **c;
+    cell *c;
     node *next;
 };
 
-class cell {
+class cell { // МОЖНО УБРАТЬ, БАЗОВЫЙ КЛАСС ЗДОРОВОЙ КЛЕТКИ
 public:
     virtual int Type() const = 0;
     virtual cell *NextStage() = 0;
@@ -31,7 +32,7 @@ protected:
 class healthy: public cell {
 public:
     healthy(int x, int y) { coord.x = x; coord.y = y; }
-    cell *NextStage() {return NULL;}
+    cell *NextStage() { return NULL; }
     int Type() const { return 1; }
 };
 
@@ -42,9 +43,10 @@ public:
     int Type() const { return 2; }
 protected:
     int count;
+    void Infect(material &X);
 };
 
-class immunity: public cell {
+class immunity: public cell { // НАСЛЕДОВАТЬСЯ ОТ ИНФИЦИРОВАННОЙ
 public:
     immunity(int x, int y) { coord.x = x; coord.y = y; count = 4; }
     cell *NextStage();
@@ -53,22 +55,24 @@ protected:
     int count;
 };
 
-class material {
+class material { // ГРАНИЦЫ В КОНСТРУКТОРЕ
 public:
     material();
     ~material();
     cell *GetCell(int x, int y) { return m[x][y]; }
     node *Clean(node *head);
-    
-    
+    void Print() const;
+    void Start();
+    void TailCut(cell *h);
+    void BecomeInfected();
 private:
-    node *AddHead(node *head, cell **_c); // добавить голову
-    node *Delete(node *t, cell **_c); // удалить элемент по значению
+    node *AddHead(node *head, cell *_c); // добавить голову
+    node *Delete(node *t, cell *_c); // удалить элемент по значению
     cell *m[SZ][SZ];
     node *head;
 };
 
-node *material::AddHead(node *head, cell **_c) {
+node *material::AddHead(node *head, cell *_c) {
     node *p = new node;
     
     if (head) {
@@ -82,7 +86,7 @@ node *material::AddHead(node *head, cell **_c) {
     return p;
 }
 
-node *material::Delete(node *t, cell **_c) {
+node *material::Delete(node *t, cell *_c) {
     node *p = t;
     
     if (t->c == _c) { // если это голова
@@ -100,20 +104,74 @@ node *material::Delete(node *t, cell **_c) {
     return t;
 }
 
+void infected::Infect(material &X) {
+    int lim = 8;
+    while (lim > 0) {
+        //coord.x
+        //coord.y
+    }
+}
+
+cell *infected::NextStage() {
+    // запускаем метод заражения соседних клеток
+    // отнимаем счетчик
+    // если счетчик равен 0, то клетка становится имунной
+}
+
+cell *immunity::NextStage() {
+    // отнимаем счетчик
+    // если равен нулю, то клетка становится здорвой, ура!!!
+}
+
+void material::Print() const {
+    int a;
+    
+    for(int i = 0; i < SZ; i++) {
+        for(int j = 0; j < SZ; j++) {
+            a = m[i][j]->Type();
+            if (a == 1)
+                cout << "🌝";
+            if (a == 2)
+                cout << "🌚";
+            if (a == 3)
+                cout << "🌎";
+        }
+        cout << endl;
+    }
+}
+
 material::material() {
     int n = SZ/2;
     
-    for (int i = 0; i <= n; i++)
+    for (int i = 0; i <= n; i++) // до центральной
         for (int j = 0; j < n; j++)
             m[i][j] = new healthy(i, j);
     
-    m[n][n] = new infected(n, n);
+    m[n][n] = new infected(n, n); // центральная
     
-    for (int j = n + 1; j < SZ; j++)
+    for (int j = n + 1; j < SZ; j++) // центральная строка заполняется до конца
         m[n][j] = new healthy(n, j);
     
-    for (int i = n + 1; i < SZ; i++)
+    for (int i = n + 1; i < SZ; i++) // все последующее
         for (int j = 0; j < SZ; j++)
             m[i][j] = new healthy(i, j);
-        
+}
+
+void material::Start() { // вот тут-то все и происходит
+    node *temporary;
+    cell *e, *temp;
+    
+    while (head) {
+        temporary = head;
+        while (temporary) {
+            temp = temporary->c;
+            
+        }
+    }
+}
+
+int main() {
+    material Mat;
+    Mat.Print();
+    Mat.Start();
 }
